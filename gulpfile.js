64645ea3eacb6b01e9a876ecfc7ce9d9d5cb72f2
snvoke <snvoke@gmail.com>
2017-06-13 23:31:49 +0400

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync');
+const reload = browserSync.reload;
+
+const paths = {
+  html: 'src/*.html',
+  scss: 'src/sass/**/*.scss',
+  js: 'src/js/main.js'
+}
+
+gulp.task('sass', () => {
+  return gulp.src(paths.scss)
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('src/css'))
+    .pipe(reload({stream:true}));
+});
+
+gulp.task('html', () => {
+  gulp.src(paths.html)
+    .pipe(reload({stream:true}));
+});
+
+gulp.task('browserSync', () => {
+  browserSync({
+    server: {
+      baseDir: "./src"
+    },
+    port: 3000,
+    // tunnel: true,
+    notify: false
+  });
+});
+
+gulp.task('watcher', () => {
+  gulp.watch(paths.scss, ['sass']);
+  gulp.watch(paths.html, ['html']);
+});
+
+gulp.task('default', ['watcher', 'browserSync']);
