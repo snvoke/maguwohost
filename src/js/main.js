72019a57a3b5d22883c75a511982fcce6379d4f6
snvoke <snvoke@gmail.com>
2017-07-03 03:18:30 +0400

    Добавлены: адаптивность с резиновой сеткой, flexbox, minireset, удалены jquery - fontawesome - normalize

--- src/js/main.js
+++ src/js/main.js
@@ -1,25 +1,59 @@
-// var btn = document.querySelector('.to-top');
-//
-// btn.addEventListener('click', function(event) {
-//   event.preventDefault();
-//   document.body.scrollTop = 0; // For Chrome, Safari and Opera
-//   document.documentElement.scrollTop = 0; // For IE and Firefox
-// });
+var toTop = document.querySelector('.to-top');
+var iconMenu = document.querySelector('.hamburger');
+var menu = document.querySelector('.nav');
 
-$(window).scroll(function() {
-    var height = $(window).scrollTop();
-    if (height > 600) {
-        $('.to-top').fadeIn();
-    } else {
-        $('.to-top').fadeOut();
-    }
-});
+iconMenu.addEventListener('click', function(event) {
+  event.preventDefault();
+  iconMenu.classList.toggle('hamburger--active');
+  menu.classList.toggle('nav--show');
+})
 
-$(document).ready(function() {
-    $(".to-top").click(function(event) {
-        event.preventDefault();
-        $("html, body").animate({ scrollTop: 0 }, "slow");
-        return false;
-    });
+function scrollToTop(scrollDuration) {
+  const   scrollHeight = window.scrollY,
+          scrollStep = Math.PI / ( scrollDuration / 15 ),
+          cosParameter = scrollHeight / 2;
 
+  var     scrollCount = 0,
+          scrollMargin,
+          scrollInterval = setInterval( function() {
+          if ( window.scrollY != 0 ) {
+              scrollCount = scrollCount + 1;
+              scrollMargin = cosParameter - cosParameter * Math.cos( scrollCount * scrollStep );
+              window.scrollTo( 0, ( scrollHeight - scrollMargin ) );
+          }
+          else clearInterval(scrollInterval);
+        }, 15 );
+}
+
+var scrollObject = {};
+window.onscroll = getScrollPosition;
+
+function getScrollPosition(){
+  scrollObject = {
+    x: window.pageXOffset,
+    y: window.pageYOffset
+  }
+  // If you want to check distance
+  if(scrollObject.y > 600) {
+    // add class
+    show();
+  } else {
+    // remove class
+    hide()
+  }
+}
+
+function hide() {
+  toTop.classList.add("to-top--hidden");
+}
+
+function show() {
+  toTop.classList.remove("to-top--hidden");
+}
+
+toTop.addEventListener('click', function(event) {
+  event.preventDefault();
+  scrollToTop(1500);
+  // document.body.scrollTop = 0; // For Chrome, Safari and Opera
+  // document.documentElement.scrollTop = 0; // For IE and Firefox
 });
